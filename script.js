581820544f3e6c36b4a423ef6652e6486bc2d5a3
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-17 15:01:21 -0500

    added function to make sure page loads first and then started adding pseudocode for what code is needed

---
+++ script.js
@@ -0,0 +1,23 @@
+//make sure the page has loaded before starting anything
+$(document).ready(function() {
+
+    //on load, hide the quiz and the breed information
+
+
+    //create global variables
+
+    //create on-click function to open the quiz, and hide the button to start the quiz
+
+    //create on-click function for the submit button for the quiz, and hide the quiz to display results info
+    //make api call to get the adoption info from results of the quiz
+
+
+    //create on-click function for viewing breed information from the nav bar, and hide the quiz and results to display breed info
+    //make api call for the breed info
+
+
+
+
+
+
+})
