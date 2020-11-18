a6051c0b081504ae5665f738df6d660592193865
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-17 20:03:09 -0500

    added functions to hide quiz content and breed info upon page load, then commented them out. also added function to show the quiz and hide the quiz button upon clicking to take the quiz- these are also commented out. all items have placeholders for the id's that will come from the index.html

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
 $(document).ready(function() {
 
     //on load, hide the quiz and the breed information
-
+    // $("####quizcontent####").hide();
+    // $("####breeedinfo####").hide();
 
     //create global variables
 
-    //create on-click function to open the quiz, and hide the button to start the quiz
+    //create on-click function to open the quiz
+    // $("####quizbutton####").on("click", function(event) {
+    //     event.preventDefault();
+    //     //show quiz content and hide the button to start the quiz
+    //     $("####quizcontent####").show();
+    //     $("####quizbutton####").hide();
+    // });
 
     //create on-click function for the submit button for the quiz, and hide the quiz to display results info
     //make api call to get the adoption info from results of the quiz
