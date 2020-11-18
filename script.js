e86ad45c49949a55b624df5502cd220ddf5eeefe
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-17 23:33:28 -0500

    added variable for the url for the first api call. started ajax api call on the js

--- script.js
+++ script.js
@@ -25,58 +25,67 @@ $(document).ready(function() {
         //make api call to get the adoption info from results of the quiz
 
         //create variables for the results that come in from the quiz
-        var age = []
-        var gender = []
-        var size = []
+        var age = [];
+        var gender = [];
+        var size = [];
 
         //create if statements to change the value of age depending on which age is checked
         if ($(".baby").is(":checked")) {
             age.push("baby")
-        }
+        };
         if ($(".young").is(":checked")) {
             age.push("young")
-        }
+        };
         if ($(".adult").is(":checked")) {
             age.push("adult")
-        }
+        };
         if ($(".senior").is(":checked")) {
             age.push("senior")
-        }
-        console.log(age)
+        };
+        console.log(age);
 
         //create if statements to change the value of gender depending on which gender is checked
         if ($(".male").is(":checked")) {
             gender.push("male")
-        }
+        };
         if ($(".female").is(":checked")) {
             gender.push("female")
-        }
+        };
         if ($(".either").is(":checked")) {
-            gender.push("male")
+            gender.push("male");
             gender.push("female")
-        }
-        console.log(gender)
+        };
+        console.log(gender);
 
         //create if statements to change the value of size depending on which size is checked
         if ($(".small").is(":checked")) {
             size.push("small")
-        }
+        };
         if ($(".medium").is(":checked")) {
             size.push("medium")
-        }
+        };
         if ($(".large").is(":checked")) {
             size.push("large")
-        }
+        };
         if ($(".X-large").is(":checked")) {
             size.push("x-large")
-        }
-        console.log(size)
+        };
+        console.log(size);
 
+        //make a variable for the url to be used in the api call
+        var adoptURL = "https://petproxy.herokuapp.com/animals";
         
+        //make api call for the breed info
+        $.ajax({
+            url: adoptURL,
+            method: "GET",
+        }).then (function(response) {
+            console.log(response)
+        });
+
     })
 
 
-    //make api call for the breed info
 
 
 
