93ef4779c86df09dad008538f479d757229a294a
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-18 13:08:54 -0500

    adjusted for merge conflicts

--- script.js
+++ script.js
@@ -9,12 +9,6 @@ $(document).ready(function () {
     //on load, hide the quiz
     $("#quiz").hide();
 
-<<<<<<< HEAD
-    //create global variables for the results that come in from the quiz
-    var age = [];
-    var gender = [];
-    var size = [];
-=======
     //function that builds url based on filtered search
     function buildQueryURL() {
 
@@ -38,7 +32,6 @@ $(document).ready(function () {
         return queryURL +"&"+ $.param(queryParams);
     }
 
->>>>>>> 3440c1a3f5ceada9a15e35ce69c3cb41fab170fc
 
     // create on-click function to open the quiz
     $("#startQuiz").on("click", function (event) {
@@ -56,13 +49,9 @@ $(document).ready(function () {
         //hide the quiz content
         $("#quiz").hide();
 
-<<<<<<< HEAD
-        
-=======
         //create variables for the results that come in from the quiz
 
 
->>>>>>> 3440c1a3f5ceada9a15e35ce69c3cb41fab170fc
         //create if statements to change the value of age depending on which age is checked
         if ($(".baby").is(":checked")) {
             age.push("baby")
@@ -77,7 +66,7 @@ $(document).ready(function () {
             age.push("senior")
         };
         console.log(age);
-        
+
         //create if statements to change the value of gender depending on which gender is checked
         if ($(".male").is(":checked")) {
             gender.push("male")
@@ -105,63 +94,15 @@ $(document).ready(function () {
             size.push("x-large")
         };
         console.log(size);
-<<<<<<< HEAD
-        
-        //make a variable for the url to be used in the api call
-        var adoptURL = "https://petproxy.herokuapp.com/animals";
-        
-        //make api call to get the adoption info from results of the quiz
-=======
 
         //make api call to get the adoption info from results of the quiz
         var queryURL = buildQueryURL();
         
->>>>>>> 3440c1a3f5ceada9a15e35ce69c3cb41fab170fc
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
             console.log(response)
-<<<<<<< HEAD
-            
-        });
-        
-        //add var from results from api call
-        var dogNameResult = response.animals[i].name
-        var dogTypeResult = response.animals[i].breeds.primary
-        var ageResult = response.animals[i].age
-        var genderResult = response.animals[i].gender
-        var sizeResult = response.animals[i].size
-        var pictureResult = response.animals[i].photos[0].medium
-        var cityStateResult = response.animals[i].contact.address.city + ", " + response.animals[i].contact.address.state
-        var linkResult = response.animals[i].url
-        //add looped tiles for results from the api call 
-        
-
-        //add adoption center name
-        //add city and state of adoption center
-        //add link to center
-        //add dog info
-        //add save button
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //make api call for the breed info
-    })
-    
-    
-    
-    
-    
-    
-    
-=======
 
         });
 
@@ -180,6 +121,5 @@ $(document).ready(function () {
 
     });
 
->>>>>>> 3440c1a3f5ceada9a15e35ce69c3cb41fab170fc
 
 });
