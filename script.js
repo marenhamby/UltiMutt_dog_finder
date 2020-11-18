fc7983fb1381b1d249a9cfc006b3d9a96719e3bc
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-17 22:17:33 -0500

    adjusted code to include the actual id's that have been added to the html

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 //make sure the page has loaded before starting anything
 $(document).ready(function() {
 
-    //on load, hide the quiz and the breed information
-    // $("####quizcontent####").hide();
-    // $("####breeedinfo####").hide();
+    //on load, hide the quiz
+    $("#quiz").hide();
 
     //create global variables
 
-    //create on-click function to open the quiz
-    // $("####quizbutton####").on("click", function(event) {
-    //     event.preventDefault();
-    //     //show quiz content and hide the button to start the quiz
-    //     $("####quizcontent####").show();
-    //     $("####quizbutton####").hide();
-    // });
+    // create on-click function to open the quiz
+    $("#startQuiz").on("click", function(event) {
+        event.preventDefault();
+        //show quiz content and hide the button to start the quiz
+        $("#quiz").show();
+        $("#startQuiz").hide();
+    });
 
     //create on-click function for the submit button for the quiz, and hide the quiz to display results info
     //make api call to get the adoption info from results of the quiz
